1236300d410a6af1bc5d0975638aaa46c0c3e30f
md.munna <mk8761174@gmail.com>
2025-01-03 14:32:16 +0600

    final project part 4 paractice done

--- src/pages/Dashboard/Carts.jsx
+++ src/pages/Dashboard/Carts.jsx
@@ -1,13 +1,43 @@
+import Swal from "sweetalert2";
 import useCart from "../../hooks/useCart";
 
-
+import { FaRegTrashAlt } from "react-icons/fa";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 const Carts = () => {
-    const [cart]=useCart()
+    const [cart,refetch]=useCart()
+    const axiosSecure=useAxiosSecure()
     const totalPrice=cart.reduce((total,item)=>total+item.price,0)
+    const handleDelete=id=>{
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+        
+          axiosSecure.delete(`/carts/${id}`)
+          .then(res => {
+            // console.log(res.data);
+            if (res.data.deletedCount> 0) {
+              refetch();
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          })
+        }
+      });
+    }
     return (
         <div>
         <div className=" flex justify-evenly">
-            <h2 className="text-2xl">items:{cart.length}</h2>
+            <h2 className="text-2xl">items:{cart?.length}</h2>
             <h2 className="text-2xl">Total Price:{totalPrice}</h2>
             <button className="btn btn-warning">Pay</button>
         </div>
@@ -31,7 +61,7 @@ const Carts = () => {
       {
         cart.map((item,index)=> <tr key={item._id}>
             <th>
-             {index}
+             {index+1}
             </th>
             <td> 
               <div className="flex items-center gap-3">
@@ -54,7 +84,8 @@ const Carts = () => {
             </td>
             <td>{item.price}</td>
             <th>
-              <button className="btn btn-ghost btn-xs">details</button>
+              <button onClick={()=>handleDelete(item._id)} className="btn btn-ghost btn-xs text-2xl">
+              <FaRegTrashAlt /></button>
             </th>
           </tr>)
       }
